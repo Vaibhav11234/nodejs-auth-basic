3a3d295723cad2dfc1600b72459e8713ce163506
Vaibhav Ghadge <vaibhavghadge42@gmail.com>
2026-01-16 18:00:16 +0530

    made changes in package json file

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const MONGO_URL = process.env.MONGO_URL;
 
 const connectToDB = async ()=>{
-    await mongoose.connect("mongodb+srv://vaibhavghadge42_db_user:vaibhavghadge44_db_user@cluster0.ashbcz9.mongodb.net/").then(
+    await mongoose.connect(MONGO_URL).then(
         ()=> console.log("Database connected successfully")
     ).catch((e)=>{
         console.log("Database connection failed",e);
